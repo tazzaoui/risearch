cb6787c8470da86c4828cdb189a9d4d8c5c64498
Taha Azzaoui <taha@azzaoui.org>
2020-01-07 20:40:16 -0500

    Read image and extract SIFT keypoints

--- main.go
+++ main.go
@@ -1,22 +1,34 @@
 package main
 
 import (
-    "fmt"
-    "os"
-    "io/ioutil"
+	"fmt"
+	"io/ioutil"
+	"os"
+
+	"gocv.io/x/gocv"
+	"gocv.io/x/gocv/contrib"
 )
 
 func main() {
-    image_dir := "data/img"
+	image_dir := "data/img"
 
-    images, err := ioutil.ReadDir(image_dir)
+	images, err := ioutil.ReadDir(image_dir)
 
-    if err != nil {
-        fmt.Println("Please download the image data set by running get_data.sh")
-        os.Exit(1)
-    }
+	if err != nil {
+		fmt.Println("Please download the image data set by running get_data.sh")
+		os.Exit(1)
+	}
 
-    for _, img := range images {
-        fmt.Println(img.Name())
-    }
+	for _, img := range images {
+		fmt.Println(img.Name())
+
+		img := gocv.IMRead(img.Name(), gocv.IMReadGrayScale)
+		defer img.Close()
+
+		sift := contrib.NewSIFT()
+		defer sift.Close()
+
+		key_points := sift.Detect(img)
+		fmt.Println(key_points)
+	}
 }
