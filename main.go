a3c7c8d5916afc78344dc106fe2541ec37fc52d8
Taha Azzaoui <taha@azzaoui.org>
2020-01-11 23:38:34 -0500

    Extract features from input image

--- main.go
+++ main.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"gocv.io/x/gocv"
+	"gocv.io/x/gocv/contrib"
 )
 
 func main() {
-
 	if len(os.Args) <= 1 {
 		fmt.Println("Please provide an input image.")
 		return
@@ -14,5 +16,14 @@ func main() {
 
 	img_path := os.Args[1]
 
-	fmt.Println(img_path)
+	sift := contrib.NewSIFT()
+	defer sift.Close()
+
+	mat := gocv.IMRead(img_path, gocv.IMReadGrayScale)
+	defer mat.Close()
+
+	if !mat.Empty() {
+		key_pts := sift.Detect(mat)
+		fmt.Println(key_pts)
+	}
 }
