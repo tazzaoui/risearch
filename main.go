7d3a06c68ab6b4d45398e3c07c15e599f3864d87
Taha Azzaoui <taha@azzaoui.org>
2020-01-15 15:53:46 -0500

    Read descriptors from disk

--- main.go
+++ main.go
@@ -1,29 +1,47 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"os"
-
-	"gocv.io/x/gocv"
-	"gocv.io/x/gocv/contrib"
+	"path"
 )
 
+func read_descs(max int) [][][]float64 {
+	kp_dir := "data/kp"
+	files, err := ioutil.ReadDir(kp_dir)
+
+	if err != nil {
+		fmt.Println("Keypoints should be in data/kp")
+		os.Exit(1)
+	}
+
+	var descs [][][]float64
+
+	i := 0
+	for _, f := range files {
+		if max > 0 && i >= max {
+			break
+		}
+		kp_path := path.Join(kp_dir, f.Name())
+		var tmp [][]float64
+		file, _ := ioutil.ReadFile(kp_path)
+		_ = json.Unmarshal([]byte(file), &tmp)
+		descs = append(descs, tmp)
+		i++
+	}
+	return descs
+}
+
 func main() {
-	if len(os.Args) <= 1 {
-		fmt.Println("Please provide an input image.")
-		return
-	}
+	/*
+		if len(os.Args) <= 1 {
+			fmt.Println("Please provide an input image.")
+			return
+		}
+	*/
 
-	img_path := os.Args[1]
-
-	sift := contrib.NewSIFT()
-	defer sift.Close()
-
-	mat := gocv.IMRead(img_path, gocv.IMReadGrayScale)
-	defer mat.Close()
-
-	if !mat.Empty() {
-		key_pts := sift.Detect(mat)
-		fmt.Println(key_pts)
-	}
+	descs := read_descs(5)
+	fmt.Println(descs)
 }
