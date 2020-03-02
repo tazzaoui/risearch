a744d390f23887a27f452cc50cd7daa348f0121f
Taha Azzaoui <taha@azzaoui.org>
2020-03-01 21:20:53 -0500

    Modularize point extraction

--- main.go
+++ main.go
@@ -16,28 +16,22 @@ type match struct {
 }
 
 const MAX_IMAGES = 100
+const KP_DIR = "data/kp"
 const K = 4
 
-func main() {
-	if len(os.Args) <= 1 {
-		fmt.Println("Please provide an input image.")
-		return
-	}
-
-	kp_dir := "data/kp"
-	files, err := ioutil.ReadDir(kp_dir)
-
-	if err != nil {
-		fmt.Println("Keypoints should be in data/kp")
-		os.Exit(1)
-	}
-
-	img_desc := lib.GetDescriptors(os.Args[1])
+func get_matches(img_path string) []match {
+	img_desc := lib.GetDescriptors(img_path)
 
 	bf := gocv.NewBFMatcherWithParams(gocv.NormL1, false)
 	defer bf.Close()
 
 	var matches []match
+
+	files, err := ioutil.ReadDir(KP_DIR)
+	if err != nil {
+		fmt.Println("Keypoints should be in data/kp")
+		os.Exit(1)
+	}
 
 	i := 0
 	for _, f := range files {
@@ -45,7 +39,7 @@ func main() {
 			break
 		}
 
-		kp_path := path.Join(kp_dir, f.Name())
+		kp_path := path.Join(KP_DIR, f.Name())
 		desc := gocv.IMRead(kp_path, gocv.IMReadUnchanged)
 
 		desc_matches := bf.KnnMatch(desc, img_desc, K)
@@ -64,6 +58,17 @@ func main() {
 	sort.Slice(matches, func(i, j int) bool {
 		return matches[i].avg_dist < matches[j].avg_dist
 	})
+
+	return matches
+}
+
+func main() {
+	if len(os.Args) <= 1 {
+		fmt.Println("Please provide an input image.")
+		return
+	}
+
+	matches := get_matches(os.Args[1])
 
 	fmt.Println("After Sorting Top 10 matches...")
 	for _, m := range matches {
