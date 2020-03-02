2fe26260ec59f3dcee6739d5c7c7f3cc08aca49f
Taha Azzaoui <taha@azzaoui.org>
2020-03-01 22:29:36 -0500

    handle gallery generation in template

--- server.go
+++ server.go
@@ -3,12 +3,17 @@ package main
 import (
 	"fmt"
 	"github.com/tazzaoui/risearch/lib"
+	"html/template"
 	"io/ioutil"
 	"net/http"
 	"path/filepath"
 )
 
-func upload_file(w http.ResponseWriter, r *http.Request) {
+type data struct {
+	Matches []lib.Match
+}
+
+func UploadFile(w http.ResponseWriter, r *http.Request) {
 	fmt.Println("[+] get_file_path")
 	r.ParseMultipartForm(32 << 20)
 
@@ -32,13 +37,22 @@ func upload_file(w http.ResponseWriter, r *http.Request) {
 	target_path := "data/target" + filepath.Ext(handler.Filename)
 	ioutil.WriteFile(target_path, bytes, 0777)
 
+	template := template.Must(template.ParseFiles("templates/results.html"))
+
 	matches := lib.GetMatches(target_path)
 	fmt.Println(matches)
+
+	err = template.ExecuteTemplate(w, "results.html", data{matches})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
 }
 
 func main() {
-	http.Handle("/", http.FileServer(http.Dir("html")))
-	http.HandleFunc("/search", upload_file)
+	http.Handle("/", http.FileServer(http.Dir("templates")))
+	http.HandleFunc("/search", UploadFile)
 	fmt.Println("[+] Listening on port 8080...")
 	http.ListenAndServe(":8080", nil)
 }
